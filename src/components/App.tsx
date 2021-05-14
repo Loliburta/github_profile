5f2608ed3aea78e4facd53d1f72b69cd328feb37
Loliburta <mateusz.maranowski@protonmail.com>
2021-05-14 22:59:04 +0200

    added fetching user data

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,29 @@
-
+import { useState } from "react";
 export const App = () => {
-  return <div>test</div>;
+  const [data, setData] = useState({});
+  const [username, setUsername] = useState("");
+  const [repositories, setRepositories] = useState([]);
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+  const submitHandler = async (
+    event: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement>
+  ) => {
+    event.preventDefault();
+    const profile = await fetch(`https://api.github.com/users/${username}`);
+    const profileJson = await profile.json();
+    console.log(profileJson);
+    const repositories = await fetch(profileJson.repos_url);
+    const reposJson = await repositories.json();
+    console.log(reposJson);
+  };
+  return (
+    <div>
+      <input type="text" value={username} onChange={onChangeHandler}></input>
+      <button type="submit" onClick={submitHandler}>
+        Search
+      </button>
+    </div>
+  );
 };
