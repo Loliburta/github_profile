4f19441e26f3f2094312edeee271773d25da28fc
Loliburta <mateusz.maranowski@protonmail.com>
2021-05-19 20:19:39 +0200

    repos styling done

--- src/components/repositories/Repositories.tsx
+++ src/components/repositories/Repositories.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { LanguageColors } from "../../utilities/LanguageColors";
+import { Repo } from "./repo/Repo";
 interface Props {
   repositories: {
     name: string;
+    html_url?: string;
     description?: string;
     language: string | null;
     stargazers_count: number;
@@ -20,18 +23,27 @@ export const Repositories: React.FC<Props> = ({ repositories }) => {
   console.log(filteredRepos);
   return (
     <div className="profile__repos">
-      <div className="profile__repos__title">Top Repositories</div>
+      <div className="profile__repos__head">
+        <div className="profile__repos__head__title">Top Repositories</div>
+        <div className="profile__repos__head__menu">menu</div>
+      </div>
       <div className="profile__repos__cards">
         {filteredRepos.map((repo) => {
+          let color = "yellow";
+          if (repo.language) {
+            color = LanguageColors[repo.language];
+          }
           return (
-            <div className="profile__repos__cards__card">
-              <div>{repo.name}</div>
-              <div>{repo.description}</div>
-              <div>{repo.language}</div>
-              <div>{repo.stargazers_count}</div>
-              <div>{repo.forks_count}</div>
-              <div>{repo.size}</div>
-            </div>
+            <Repo
+              name={repo.name}
+              html_url={repo.html_url}
+              description={repo.description}
+              language={repo.language}
+              stargazers_count={repo.stargazers_count}
+              forks_count={repo.forks_count}
+              size={repo.size}
+              color={color}
+            />
           );
         })}
       </div>
---
+++ src/components/repositories/repo/Repo.tsx
@@ -0,0 +1,68 @@
+import { Icon } from "@iconify/react";
+import star from "@iconify-icons/ant-design/star-outlined";
+import fork from "@iconify-icons/jam/fork";
+interface Props {
+  name: string;
+  html_url?: string;
+  description?: string;
+  language: string | null;
+  stargazers_count: number;
+  forks_count?: number;
+  size?: number;
+  color: string;
+}
+export const Repo: React.FC<Props> = ({
+  name,
+  html_url,
+  description,
+  language,
+  stargazers_count,
+  forks_count,
+  size,
+  color,
+}) => {
+  return (
+    <div className="profile__repos__cards__card">
+      <a href={html_url} className="profile__repos__cards__card__title">
+        {name}
+      </a>
+      <div className="profile__repos__cards__card__description">
+        {description}
+      </div>
+      <div className="profile__repos__cards__card__footer">
+        <div className="profile__repos__cards__card__footer__info">
+          <div className="profile__repos__cards__card__footer__info__group">
+            <div className="profile__repos__cards__card__footer__info__group__language">
+              <div
+                className="profile__repos__cards__card__footer__info__group__language__color"
+                style={{
+                  backgroundColor: color,
+                }}
+              ></div>
+              <span className="profile__repos__cards__card__footer__info__group__language__text">
+                {language}
+              </span>
+            </div>
+            <div className="profile__repos__cards__card__footer__info__group__stars">
+              <Icon
+                icon={star}
+                className="profile__repos__cards__card__footer__info__group__stars__icon"
+              />
+              {stargazers_count}
+            </div>
+            <div className="profile__repos__cards__card__footer__info__group__forks">
+              <Icon
+                icon={fork}
+                className="profile__repos__cards__card__footer__info__group__forks__icon"
+              />
+              {forks_count}
+            </div>
+          </div>
+          <div className="profile__repos__cards__card__footer__info__size">
+            {size?.toLocaleString()} KB
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
